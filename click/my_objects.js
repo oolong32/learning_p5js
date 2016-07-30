58ab3dca4aa19ca51d63daccf5603a5dcf6bed4e
Josef Renner <josef.renner@gmail.com>
2016-07-30 23:59:53 +0200

    Add switch for different shapes

--- click/my_objects.js
+++ click/my_objects.js
@@ -3,13 +3,26 @@ function Lllp( x, y ) {
   this.y = y;
   this.r = 5;
   this.t = 1000; // Lllp’s lifetime
+  this.shape = Math.floor( random( 3 ) );
 
   this.display = function() {
-    push();
     fill( 255 );
-    stroke( 0 );
-    ellipse( this.x, this.y, this.r, this.r );
-    pop();
+    noStroke();
+    switch ( this.shape ) {
+      case 0:
+        ellipse( this.x, this.y, this.r, this.r );
+        break;
+      case 1:
+        rect( this.x, this.y, this.r, this.r );
+        break;
+      case 2:
+        push();
+        stroke( 255 );
+        strokeWeight( 4 );
+        line( this.x, this.y, this.x + this.r, this.y + this.r );
+        pop();
+        break;
+    }
   };
 
   this.alive = function() {
