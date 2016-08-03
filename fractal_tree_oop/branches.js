9098d0a35f1c363db8a9ff2bb016d79c80a0e015
Josef Renner <josef.renner@gmail.com>
2016-08-04 00:22:50 +0200

    Fractal tree generation object-oriented-ly

--- fractal_tree_oop/branches.js
+++ fractal_tree_oop/branches.js
@@ -1,4 +1,34 @@
 function Branch( begin, end ) {
   this.begin = begin;
   this.end = end;
+  this.grown = false;
+
+  this.show = function() {
+    stroke( 255 );
+    line( this.begin.x, this.begin.y, this.end.x, this.end.y );
+  }
+
+  this.jitter = function() {
+    this.end.x += random( -1, 1 );
+    this.end.y += random( -1, 1 );
+  }
+  this.branchA = function() {
+    var dir = p5.Vector.sub( this.end, this.begin );
+    var v = random( 3, 7 );
+    dir.rotate( PI / v );
+    dir.mult( 0.67 );
+    var newEnd = p5.Vector.add( this.end, dir );
+    var a = new Branch( this.end, newEnd )
+    return a;
+  }
+
+  this.branchB = function() {
+    var dir = p5.Vector.sub( this.end, this.begin );
+    var v = random( 3, 7 );
+    dir.rotate( -PI / v );
+    dir.mult( 0.67 );
+    var newEnd = p5.Vector.add( this.end, dir );
+    var b = new Branch( this.end, newEnd )
+    return b;
+  }
 }
--- fractal_tree_oop/sketch.js
+++ fractal_tree_oop/sketch.js
@@ -1,29 +1,50 @@
-var angle;
-var slider;
+var tree = [];
+var leaves = [];
+var count = 0;
 
 function setup() {
-  createCanvas( 300, 200 );
+  createCanvas( 400, 300 );
   pixelDensity( 1 );
-  slider = createSlider( 0, TWO_PI, Math.PI / 4, 0.01 );
+
+  // slider = createSlider( 0, TWO_PI, Math.PI / 4, 0.01 );
+
+  var a = createVector( width / 2, height );
+  var b = createVector( width / 2, height - 100 );
+  var root = new Branch( a, b );
+
+  tree[ 0 ] = root;
+}
+
+function mousePressed() {
+  for ( var i = tree.length - 1; i >= 0; i-- ) {
+    if ( !tree[ i ].grown ) {
+      tree.push( tree[ i ].branchA() );
+      tree.push( tree[ i ].branchB() );
+    }
+    tree[ i ].grown = true;
+  }
+  count += 1;
+
+  if ( count === 6 ) {
+    for ( var j = 0; j < tree.length; j++ ) {
+      if ( !tree[ j ].grown ) {
+        var leaf = tree[ j ].end.copy();
+        leaves.push( leaf );
+      }
+    }
+  }
 }
 
 function draw() {
   background( 51 );
-  stroke( 0, 255, 100 );
-  angle = slider.value();
-  translate( 100, height);
-  branch( 62 );
-}
-
-function branch( len ) {
-  line( 0, 0, 0, -len );
-  translate( 0, -len);
-  if ( len > 1 ) {
-    push();
-    rotate( angle )
-    branch( len * 0.67 );
-    pop();
-    rotate( -angle );
-    branch( len * 0.67);
+  // angle = slider.value();
+  for ( var i = 0; i < tree.length; i++ ) {
+    // tree[ i ].jitter();
+    tree[ i ].show(); 
+  }
+  for ( var i = 0; i < leaves.length; i++ ) {
+    fill( 0, 255, 255, 177 );
+    noStroke();
+    ellipse( leaves[ i ].x, leaves[ i ].y, 8, 8 );
   }
 }
