21d6e6a86dbbc44d4a47432526b5a4bda7b46659
Josef Renner <josef.renner@gmail.com>
2016-12-20 23:02:09 +0100

    not sure what happened here

--- madlib_generator/sketch.js
+++ madlib_generator/sketch.js
@@ -1,12 +1,27 @@
 var data;
+var txt = '$$exclamation$$! they said $$adverb$$ as they jumped into their $$noun$$ and flew off with their $$adjective$$ $$pluralnoun$$.';
 
 function setup() {
   noCanvas();
-  Tabletop.init( { key: '1pW3pRp4eZqr5t8Iiq328xGzRvVIXV7J6D_9B-yUIoSg',
+  Tabletop.init( { key: '1NQmPjdyJFjA2GpD5hW1ybLOAV2M_ACHoDsxYywJwWrw',
   callback: gotData,
   simpleSheet: true } );
+
+  var button = createButton( 'generate MadLib' );
+  button.mousePressed( generate );
 }
 
 function gotData( stuff, tabletop ) {
   data = stuff;
+}
+
+function replacer( match, pos ) {
+  var entry = random( data );
+  return entry[ pos ];
+}
+
+function generate(  ) {
+  // console.log( 'generate' );
+  var madlib = txt.replace( /\$\$(.*?)\$\$/g, replacer );
+  createP( madlib );
 }
