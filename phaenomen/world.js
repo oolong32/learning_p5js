90b436fd9448476ae5ad2af99aebbc0cfb81ea14
Josef Renner <josef.renner@gmail.com>
2017-04-28 14:52:18 +0200

    Add some comments before undertaking project ‘blob’

--- phaenomen/world.js
+++ phaenomen/world.js
@@ -4,9 +4,9 @@ function World(particles, phenomena) {
   this.particles = [];
   this.phenomena = [];
   this.active_phenomenon = null;
+  this.ctr = Math.random() * 1000; // für noise
 
   this.initialize = function() {
-    // puh …
     var step = 360 / this.num_particles;
     var num = 0; // each particle gets a number
     for (var i = 0; i < 360; i += step) {
@@ -32,6 +32,45 @@ function World(particles, phenomena) {
     this.active_phenomenon = this.phenomena[this.phenomena.length-1];
   };
 
+
+  // es braucht neu: initialize, ohne Postitionierung
+  // dann. this.updatePosition mit der noise funktion,
+  // damit jedes mal die pos. der partikel upgedatet wird.
+  // dann könnte beim initialisieren einmal diese positionierung
+  // vorgenommen werden.
+
+  this.initializeAsBlob = function() {
+    // … puh … ändern gemäss Blob
+    var step = 360 / this.num_particles;
+    var num = 0; // each particle gets a number
+    var xoff = 0;
+    for (var i = 0; i < 360; i += step) {
+      var a = radians(i);
+      var x = cos(a);
+      var y = sin(a);
+      var m = 220; // magnitude
+      var v = createVector(x, y);
+      var laerm = noise(x + 1, y + 1, this.ctr);
+
+      v.mult(m);
+      var p = new Partikel(num, v, 20);
+      this.particles.push(p);
+      num += 1;
+    }
+    this.ctr += 0.01
+    // das geht so nicht, muss in ‘update’!!!
+
+    // vorerst: Phaenomenon = zufällige Auswahl Partikel.
+    for (var i = 0; i < this.num_phenomena; i++) {
+      var phenomenon = new Phenomenon((i+1) * 3);
+      phenomenon.initialize();
+      this.phenomena.push(phenomenon);
+    }
+    // total arbiträre auswahl
+    // vvvvvvvvvvvvvvvvvvvvvvv
+    this.active_phenomenon = this.phenomena[this.phenomena.length-1];
+  };
+
   this.switchPhenomenon = function() {
     // puh …
   };
@@ -42,7 +81,7 @@ function World(particles, phenomena) {
 
   this.displayParticles = function() {
     for (var i = 0; i < this.particles.length; i++) {
-      this.particles[i].repraesentieren();
+      this.particles[i].repraesentieren(); // können wir die Namen der Methoden zur Darstellung vereinheitlichen bitte?
     }
   }
 }
